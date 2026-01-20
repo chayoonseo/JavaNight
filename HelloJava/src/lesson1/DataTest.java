3037b2b7e9f54a0f7fcc4d298384637bb7a935fe
A <A@room-4-six>
2026-01-20 20:49:44 +0900

    var 키워드 사용

--- HelloJava/src/lesson1/DataTest.java
+++ HelloJava/src/lesson1/DataTest.java
@@ -11,23 +11,23 @@ public class DataTest {
 		short b = -10000;
 		System.out.println("b = " + b);
 
-		int c = 1; //속도가 빠름!
+		var c = 1; //속도가 빠름!
 		System.out.println("c = " + c);
 
-		long d = 1000L; // 크기는 가장 크지만, int보다 큰 정수를 담고 싶을 때
+		var d = 1000L; // 크기는 가장 크지만, int보다 큰 정수를 담고 싶을 때
 		System.out.println("d = " + d);
 
 		//float, double
-		float e = 1.1f; // 32비트 실수
+		var e = 1.1f; // 32비트 실수
 		System.out.println("e = " + e);
 
-		double f = 1.1111; //64비트 실수 (웬만하면 double 씀)
+		var f = 1.1111; //64비트 실수 (웬만하면 double 씀)
 		System.out.println("f = " + f);
 
-		char g = '강'; // 2바이트 문자
+		var g = '강'; // 2바이트 문자
 		System.out.println(g);
 
-		boolean h = false; // 참 거짓만을 다루는 타입(4바이트)
+		var h = false; // 참 거짓만을 다루는 타입(4바이트)
 		System.out.println(h);
 	}
 
